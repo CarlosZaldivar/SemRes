9a059352a2f65395a3ddbc237fd68623353f315d
Carlos Zaldivar Batista <carlos.zaldivar.batista@gmail.com>
2017-06-02 19:25:38 +0200

    Remove unnecessary comment.

--- src/main/java/com/github/semres/gui/IDAlreadyTakenException.java
+++ src/main/java/com/github/semres/gui/IDAlreadyTakenException.java
@@ -1,8 +1,5 @@
 package com.github.semres.gui;
 
-/**
- * Created by carlos on 4/20/17.
- */
 public class IDAlreadyTakenException extends RuntimeException {
     public IDAlreadyTakenException() {
         super("ID already taken.");
